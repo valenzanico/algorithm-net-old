0eada834c8a6873b860f2600deb6fec5a8f2485f
Nicolò Valenza <nico.vale.spazio@outlook.com>
2020-07-08 20:44:11 +0200

    upgrade sw

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'algorithm-net';
+var filesToCache = [
+'/',
+'/index.html',
+'/articoli/index.html',
+'/applicazioni/index.html',
+'/register-sw.js',
+'/stylesheets/appstyle.css',
+'/stylesheets/sitestyle.css',
+'/manifest.json'
+];
+/* Avvia il Service Worker e Memorizza il contenuto nella cache */
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open(cacheName).then(function(cache) {
+return cache.addAll(filesToCache);
+})
+);
+});
+/* Serve i Contenuti Memorizzati quando sei Offline */
+self.addEventListener('fetch', function(e) {
+e.respondWith(
+caches.match(e.request).then(function(response) {
+return response || fetch(e.request);
+})
+);
+});
